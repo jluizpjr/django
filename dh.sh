844ba73c770b74ec15b94f13b7f27c46866f2bac
Joao Luiz Pereira <jluizpjr@gmail.com>
2021-10-19 20:01:51 -0300

    First version

--- dh.sh
+++ dh.sh
@@ -1,3 +1,3 @@
-sudo docker build --tag sample01 .
-sudo docker image tag sample01 jluizpjrdockerhub/sample01:v0.1
-sudo docker image push jluizpjrdockerhub/sample01:v0.1
+sudo docker build --tag django .
+sudo docker image tag django jluizpjrdockerhub/django:v0.1
+sudo docker image push jluizpjrdockerhub/django:v0.1
